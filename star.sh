8e2c31938c9a71d9e64750bbc4e112ecb61b7172
baharjun6 <baharjun0609@gmail.com>
2026-07-15 03:07:03 +0330

    Create star.sh

---
+++ star.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+
+echo "Starting Sanaei Railway"
+
+
+export NGINX_PORT=${PORT:-3000}
+
+
+envsubst '${NGINX_PORT}' \
+< /etc/nginx/nginx.conf.template \
+> /etc/nginx/nginx.conf
+
+
+mkdir -p /etc/x-ui
+
+
+if [ ! -f /etc/x-ui/config.json ]; then
+
+echo "first start"
+
+fi
+
+
+exec supervisord -n
